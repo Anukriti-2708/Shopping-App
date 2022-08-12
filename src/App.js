b3f485996c439e8299dda9a8bbb17529302c2b5c
Anukriti-2708 <saxenaanukriti91@gmail.com>
2022-08-12 20:35:18 +0530

    Checkout page initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
 import Header from './Header';
-import Home from './Home';
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Checkout from "./Checkout";
+
 function App() {
+  
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <Header/>
+       <div className='app'>
+       
+        <Routes>
+        
+            <Route path="/" element = {<Home/>}/>
+             
+           
+            <Route path="/checkout" element ={<Checkout/>}/>
+            
+       </Routes>
+       
+      </div>
+    
+    </Router>
+    
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css"
+
 import Product from "./Product";
 
 
