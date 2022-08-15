81a11620bf0e618e2f3a8585436a442f703bc753
deekshamudgal <deekshamudgal@gmail.com>
2022-08-16 00:03:04 +0530

    Main Login Page (without Authentication and Animation functionalities)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Header from './Header';
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   
@@ -12,19 +12,23 @@ function App() {
        <div className='app'>
        
         <Routes>
-        
-            <Route path="/" element = {<Home/>}/>
-             
-           
+
+           <Route path="/" element = {<Home/>}/>
+            
             <Route path="/checkout" element ={<Checkout/>}/>
+
+            <Route path="/login" element = {<Login/>}/>
             
        </Routes>
        
       </div>
     
     </Router>
+
+
     
   );
 }
+
 
 export default App;
